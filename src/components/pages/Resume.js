940a692005586d7bf988b39a773582b7445d9b15
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-11-10 14:22:06 -0700

    Pdf dl correctly

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,7 @@ export default function Resume() {
   return (
     <h2>
       Download My
-      <a href='../assets/peoples-resume.pdf' download='Peoples-Resume.pdf'>
+      <a href='./assets/peoples-resume.pdf' download='Peoples-Resume.pdf'>
         Resume
       </a>
     </h2>
