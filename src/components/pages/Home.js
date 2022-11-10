9ce6b73c464367c5071ceafe96e815e560e7e2dd
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-11-10 15:14:40 -0700

    added pics and edited the about page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,38 @@
 export default function Home() {
   return (
     <>
-      <h2>About Me</h2>
-      <p>
-        Hi, I'm Matt, an aspiring Fullstack Developer. I graduated magna cum
-        laude from the University of Colorado-Boulder in 2011 with a degree in
-        Ecology and Evolutionary Biology with a teaching certificate in
-        Secondary Science Education. I then worked as a wildland firefighter for
-        the US Forest Service for nearly a decade, ultimately becoming a
-        helicopter rappeler. This was a fun way to spend my twenties and taught
-        me discipline, how to make decisions in high stress environments and
-        manageing people. I am excited to start a new chapter in computer
-        programming.
-      </p>
+      <div className='row about'>
+        <h2>About Me</h2>
+        <div className='col-lg-10'>
+          Hi, I'm Matt, an aspiring Fullstack Developer. I graduated magna cum
+          laude from the University of Colorado-Boulder in 2011 with a degree in
+          Ecology and Evolutionary Biology with a teaching certificate in
+          Secondary Science Education. I then worked as a wildland firefighter
+          for the US Forest Service for nearly a decade, ultimately becoming a
+          helicopter rappeler. This was a fun way to spend my twenties and
+          taught me discipline, how to make decisions in high stress
+          environments and managing people. I will have completed the full-stack
+          programming bootcamp at the University of Denver in early December
+          2022. With this new knowledge and skill set, I am excited to start a
+          new chapter in computer programming.
+        </div>
+      </div>
+      <div className='row about-img'>
+        <div className='col-lg-5 center'>
+          <img
+            src='./assets/images/res-pic.jpg'
+            alt='me with darwin'
+            className='pic'
+          />
+        </div>
+        <div className='col-lg-5 center'>
+          <img
+            src='./assets/images/me-rap.jpg'
+            alt='me with darwin'
+            className='pic'
+          />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,38 @@
 export default function Resume() {
   return (
-    <h2>
-      Download My
-      <a href='./assets/peoples-resume.pdf' download='Peoples-Resume.pdf'>
-        Resume
-      </a>
-    </h2>
+    <>
+      <h2>
+        Download My{' '}
+        <a href='./assets/peoples-resume.pdf' download='Peoples-Resume.pdf'>
+          Resume
+        </a>
+      </h2>
+      <div className='row skill-list'>
+        <div className='col-md-12 col-lg-5'>
+          <ul>
+            <h3>Front-end Proficiencies</h3>
+            <li>HTML</li>
+            <li className='list'>CSS</li>
+            <li className='list'>JavaScript</li>
+            <li className='list'>jQuery</li>
+            <li className='list'>Responsive Design</li>
+            <li className='list'>React</li>
+            <li className='list'>Bootstrap</li>
+          </ul>
+        </div>
+        <div className='col-md-12 col-lg-5'>
+          <ul>
+            <h3 className='res'>Back-end Proficiencies</h3>
+            <li className='list'>APIs</li>
+            <li className='list'>Node</li>
+            <li className='list'>Express</li>
+            <li className='list'>MySQL, Sequelize</li>
+            <li className='list'>MongoDB, Mongoose</li>
+            <li className='list'>REST</li>
+            <li className='list'>GraphQL</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
