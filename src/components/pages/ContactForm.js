25cfe190532a5218cfc5c1a4b28c3fd7c7d91c2f
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-11-14 09:40:48 -0700

    contact form done

--- src/components/pages/ContactForm.js
+++ src/components/pages/ContactForm.js
@@ -1,42 +1,118 @@
-import React from 'react';
 import { useState } from 'react';
+import { validateEmail, toTitleCase } from '../utils/helpers';
 
 function ContactForm() {
+  const defaultErrorMessage =
+    'Validation Works, does not send message yet due to no backend';
   const [fullName, setFullName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState(defaultErrorMessage);
 
   const handleInputChange = (e) => {
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    if (inputType === 'fullName') {
+    if (inputType === 'name') {
       setFullName(inputValue);
+    }
+
+    if (inputType === 'email') {
+      setEmailAddress(inputValue);
+    }
+
+    if (inputType === 'message') {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleLostFocus = (event) => {
+    const { target } = event;
+    const inputName = target.name;
+    const inputValue = target.value;
+
+    // If the field is blank, tell the user that it's required.
+    if (inputValue === '') {
+      setErrorMessage(`${toTitleCase(inputName)} is required.`);
+      return;
+    }
+
+    // If there's something in the email text box, check to see if it's a valid email address
+    if (inputName === 'email') {
+      if (!validateEmail(inputValue)) {
+        setErrorMessage('Invalid Email Address.');
+        return;
+      }
+    }
+
+    //Clear error message if inputs are valid
+    if (fullName && message && validateEmail(emailAddress)) {
+      setErrorMessage('');
     }
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    if (fullName && emailAddress && message) {
+      //Logic to send the message to me will go here, but we have no backend so I will leave it like this for now
+      alert('Message Sent!');
+      setErrorMessage(defaultErrorMessage);
+    }
 
+    //Clears form inputs
     setFullName('');
+    setEmailAddress('');
+    setMessage('');
   };
 
   return (
-    <form>
-      <div className="form-group">
-        <h2>Contact Me!</h2>
-        <label htmlFor="Full Name">Full Name</label>
-        <input
-          onChange={handleInputChange}
-          value={fullName}
-          name="fullName"
-          type="text"
-          placeholder="Full Name"
-        />
-        <button type="button" onClick={handleFormSubmit}>
-          Submit
-        </button>
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div className='form-group'>
+          <h2>Contact Me!</h2>
+          <div className='form-inputs'>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              value={fullName}
+              name='name'
+              placeholder='Name'
+              onChange={handleInputChange}
+              onBlur={handleLostFocus}
+            />
+
+            <label htmlFor='Email'>Email Address</label>
+            <input
+              type='text'
+              value={emailAddress}
+              name='email'
+              placeholder='Email Address'
+              onChange={handleInputChange}
+              onBlur={handleLostFocus}
+            />
+
+            <label htmlFor='Message'>Message</label>
+            <textarea
+              name='message'
+              value={message}
+              rows='10'
+              placeholder='Message'
+              onChange={handleInputChange}
+              onBlur={handleLostFocus}
+            />
+
+            <input type='submit' text='Submit' className='sub-btn' />
+          </div>
+        </div>
+      </form>
+      <br />
+      {errorMessage && (
+        <div>
+          <p>{errorMessage}</p>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/utils/helpers.js
@@ -0,0 +1,13 @@
+// Helper function to validate email against a regular expression
+export function validateEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Helper fxn to capitalize the first letter word
+export function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
