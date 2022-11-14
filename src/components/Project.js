1f7a13b649f8690e9927abd9e0d4cf0c799f3b40
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-11-14 08:22:36 -0700

    refactored to have a project.js that is recycled for each project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,1 +1,28 @@
-import React from 'react';
+import { Github } from 'react-bootstrap-icons';
+
+export default function Project(props) {
+  const title = props.title;
+  const url = props.url;
+  const image = props.image;
+  const description = props.description;
+  const github = props.github;
+
+  return (
+    <div className='card'>
+      <div className='card-img'>
+        <a href={url} target='_blank' rel='noreferrer noopener'>
+          <img src={image} class='card-img-top' alt='Mythic+ App' />
+        </a>
+      </div>
+      <div className='card-body'>
+        <div className='project-title'>
+          <h4 className='card-title'>{title}</h4>
+          <a href={github} target='_blank' rel='noreferrer noopener'>
+            <Github className='icon ms-2' size={50} />
+          </a>
+        </div>
+        <p className='card-text pt-2'>{description}</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ProjectData.js
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    title: 'Mythic+ Team Maker',
+    url: 'https://mythic-plus-team.herokuapp.com/',
+    image: './assets/images/mplus-ss.png',
+    description:
+      'An App to allow World of Warcraft players search for other players to construct a team with real time stats. The user can then save these teams to the websites database and edit them by logging on any device.',
+    github: 'https://github.com/peoplesm/mythic-plus-team',
+  },
+  {
+    title: 'Magic App',
+    url: 'https://peoplesm.github.io/magic-app/',
+    image: './assets/images/magic-ss.png',
+    description:
+      "This app allows Magic players to search for playing cards and save them in a wishlist stored in your browser's local storage. Users are able to search thousands of cards through a Scryfall API technology, save the chosen cards to a wishlist that the user names, attach a fun gif.",
+    github: 'https://github.com/peoplesm/magic-app',
+  },
+  {
+    title: 'Weather Dashboard',
+    url: 'https://peoplesm.github.io/weather-dashboard/',
+    image: './assets/images/weather-ss.png',
+    description:
+      "An app that allows the user to input a city and get the current weather along with a five day forecast. Also, each city is saved to a search list that can be clicked to bring up that particular citie's weather.",
+    github: 'https://github.com/peoplesm/weather-dashboard',
+  },
+  {
+    title: "Matt's Tech Blog",
+    url: 'https://guarded-chamber-72241.herokuapp.com/',
+    image: './assets/images/tech-blog-ss.png',
+    description:
+      'This project is a CMS-style blog site similar to a Wordpress site, where developers can publish their blog posts and comment on other developers posts as well.',
+    github: 'https://github.com/peoplesm/tech-blog',
+  },
+  {
+    title: 'Progressive Web App - Text Editor',
+    url: 'https://blooming-retreat-74088.herokuapp.com/',
+    image: './assets/images/jate-ss.png',
+    description:
+      'This app is a text editor that runs in the browser and features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The application will also function offline.',
+    github: 'https://github.com/peoplesm/pwa-text-editor',
+  },
+  {
+    title: 'Weather Dashboard',
+    url: 'https://evening-escarpment-24555.herokuapp.com/',
+    image: './assets/images/note-taker-ss.png',
+    description:
+      'An application that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.',
+    github: 'https://github.com/peoplesm/note-taker',
+  },
+];
